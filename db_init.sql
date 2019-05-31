a379377bdc81f53221f5c9923fdb9d983d2bb21f
夏飞 <xiafei571@gmail.com>
2019-06-01 01:51:58 +0900

    修改脚本

--- db_init.sql
+++ db_init.sql
@@ -6,29 +6,29 @@ USE setsu0406;
 
 -- 创建部门表
 CREATE TABLE `dept` (
-  `deptno` int(4) NOT NULL COMMENT '部门ID',
+  `deptno` int(4) NOT NULL AUTO_INCREMENT COMMENT '部门ID',
   `dname` varchar(14) DEFAULT NULL COMMENT '部门名称',
   `loc` varchar(13) DEFAULT NULL COMMENT '所在地',
   PRIMARY KEY (`deptno`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8;
 
 /* 修改主键为自增 */ 
-ALTER TABLE `dept` CHANGE `deptno` `deptno` INT(4)  NOT NULL  AUTO_INCREMENT  COMMENT '部门ID';
+-- ALTER TABLE `dept` CHANGE `deptno` `deptno` INT(4)  NOT NULL  AUTO_INCREMENT  COMMENT '部门ID';
 
-CREATE TABLE `student`(
-`sid` int(10) NOT NULL COMMENT '学生ID',
-`sname` varchar(10) DEFAULT NULL COMMENT '学生名前',
-`sex` int(2) DEFAULT 0 COMMENT '性別　0無知　1男　2女',
-`birth`   date DEFAULT NULL COMMENT '生年月日',
-`loc` varchar(10) DEFAULT NULL COMMENT '出身',
-`ethnic` varchar(10) DEFAULT NULL COMMENT '民族',
-PRIMARY KEY (`sid`)
-
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 学生表
+CREATE TABLE `student` (
+  `sid` int(10) NOT NULL AUTO_INCREMENT COMMENT '学生ID',
+  `sname` varchar(10) DEFAULT NULL COMMENT '学生名前',
+  `sex` int(2) DEFAULT '0' COMMENT '性別　0無知　1男　2女',
+  `birth` date DEFAULT NULL COMMENT '生年月日',
+  `loc` varchar(10) DEFAULT NULL COMMENT '出身',
+  `ethnic` varchar(10) DEFAULT NULL COMMENT '民族',
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=20190004 DEFAULT CHARSET=utf8;
 
 -- 创建员工表
 CREATE TABLE `emp` (
-  `empno` int(4) NOT NULL COMMENT '员工编号',
+  `empno` int(4) NOT NULL AUTO_INCREMENT COMMENT '员工编号',
   `ename` varchar(10) NOT NULL DEFAULT '' COMMENT '员工姓名',
   `job` varchar(10) DEFAULT NULL COMMENT '职位',
   `mgr` int(4) DEFAULT NULL COMMENT '上级ID',
@@ -36,16 +36,17 @@ CREATE TABLE `emp` (
   `sal` int(7) DEFAULT NULL COMMENT '薪资',
   `deptno` int(2) DEFAULT NULL COMMENT '部门ID',
   PRIMARY KEY (`empno`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7936 DEFAULT CHARSET=utf8;
 
+-- 图书表
 CREATE TABLE `book` (
   `bookId` int(4) NOT NULL AUTO_INCREMENT COMMENT '图书ID',
   `bname` varchar(14) DEFAULT NULL COMMENT '图书名称',
   `btype` varchar(13) DEFAULT NULL COMMENT '类型',
-  `price` decimal(10,0) DEFAULT '0' COMMENT '价格',
+  `price` decimal(10,0) DEFAULT '0' COMMENT '类型',
   `author` varchar(13) DEFAULT NULL COMMENT '作者',
   PRIMARY KEY (`bookId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=utf8;
 
 -- 初始化数据
 INSERT INTO `dept` (`deptno`, `dname`, `loc`)
@@ -55,8 +56,19 @@ VALUES
   (30, '人力资源部', '东京'),
   (40, '销售部', '大阪'),
   (50, '研究所', '京都'),
-  (60, '运营部', '名古屋');
+  (60, '运营部', '名古屋'),
+  (62, 'test2', '北京');
 
+
+INSERT INTO `book` (`bookId`, `bname`, `btype`, `price`, `author`)
+VALUES
+  (100, '《人类简史》', '历史', 35, '尤瓦尔'),
+  (101, '《哈利波特》', '小说', 50, 'JK罗琳'),
+  (102, '《平凡的世界》', '小说', 29, '陆瑶'),
+  (103, '《Java编程思想》', '技术', 99, '李教授'),
+  (104, '《时间简史》', '科普', 56, '霍金'),
+  (105, '《钢铁是怎样练成的》', '传记', 78, '聂夫托尔斯泰'),
+  (106, '小盖自传', '自传', 199, '小盖');
 
 
 INSERT INTO `emp` (`empno`, `ename`, `job`, `mgr`, `hiredate`, `sal`, `deptno`)
@@ -74,14 +86,14 @@ VALUES
   (7876, '小龙女', '测试', 7566, '2018-05-23', 1500, 20),
   (7900, '欧阳锋', '销售经理', 7839, '2018-12-03', 1950, 40),
   (7902, '李莫愁', '性能测试', 7566, '2018-12-03', 3100, 20),
-  (7934, '郭襄', '开发', 7782, '2018-01-23', 3000, 10);
+  (7934, '郭襄', '开发', 7782, '2018-01-23', 3000, 10),
+  (7935, '霍元甲', '武夫', 1001, '2019-01-01', 9000, 1001);
 
-  INSERT INTO `book` (`bookId`, `bname`, `btype`, `price`, `author`)
+
+INSERT INTO `student` (`sid`, `sname`, `sex`, `birth`, `loc`, `ethnic`)
 VALUES
-	(100, '《人类简史》', '历史', 35, '尤瓦尔'),
-	(101, '《哈利波特》', '小说', 50, 'JK罗琳'),
-	(102, '《平凡的世界》', '小说', 29, '陆瑶'),
-	(103, '《Java编程思想》', '技术', 99, '李教授'),
-	(104, '《时间简史》', '科普', 56, '霍金'),
-	(105, '《钢铁是怎样练成的》', '传记', 78, '聂夫托尔斯泰');
+  (20190001, 'tom', 0, '1980-01-01', '东京', '汉族'),
+  (20190002, 'json', 2, '1990-02-02', '北京', '满族'),
+  (20190003, '梁同学', 1, '2019-01-01', '吉林', '朝鲜族');
+
 
